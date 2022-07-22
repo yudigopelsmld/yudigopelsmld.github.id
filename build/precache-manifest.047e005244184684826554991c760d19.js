d93a31e7e024d0c93983d6c5a729c4018f909969
yudi <yudigopels@gmail.com>
2022-07-21 23:48:49 -0700

    Deploy

---
+++ build/precache-manifest.047e005244184684826554991c760d19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98362e0b30478aa1c3f2",
+    "url": "/static/css/main.087aa554.chunk.css"
+  },
+  {
+    "revision": "98362e0b30478aa1c3f2",
+    "url": "/static/js/main.98362e0b.chunk.js"
+  },
+  {
+    "revision": "7713e6cf05f1a254d230",
+    "url": "/static/js/1.7713e6cf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "773503870b5277fc53afb533a380cdf5",
+    "url": "/index.html"
+  }
+];
